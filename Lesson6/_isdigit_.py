b837d63388c7176c549d604aab46a29c0556bcee
Samwell <Samwell@gmail.com>
2023-04-19 18:41:12 +0300

    fix

--- Lesson6/_isdigit_.py
+++ Lesson6/_isdigit_.py
@@ -3,10 +3,12 @@ def checker(x: str):
         print(f"{x}: positive integer")
     elif '-' in x and x[1].isdigit() and '.' not in x:
         print(f"{x}: negative integer")
-    elif '.' in x and (x[0:x.index('.')-1] + x[x.index('.')+1]).isdigit():
+    elif '.' in x and (x[0:x.index('.')-1] + x[x.index('.')+1]).isdigit() and '-' not in x:
         print(f"{x}: positive fractional number")
-    elif '-' in x and (x[1:x.index('.')-1] + x[x.index('.')+1]).isdigit():
+    elif '-' in x and (x[1:x.index('.')-1] + x[x.index('.')+1]).isdigit() and (x[:2] != '-.' or x[:2] != '-.'):
         print(f"{x}: negative fractional number")
+    elif x[:2] == '-.' or x[:2] == '.-':
+        print(f"{x}: invalid value")
     else:
         print(f"{x}: invalid value")
 
@@ -15,5 +17,4 @@ checker("-11.7")
 checker("11.2")
 checker("-1")
 checker("11r")
-
-
+checker("-.814")
