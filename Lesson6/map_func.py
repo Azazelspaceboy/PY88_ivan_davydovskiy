74584f5eef974915a86d93788b00758878777862
Samwell <Samwell@gmail.com>
2023-04-19 12:35:50 +0300

    update

--- Lesson6/map_func.py
+++ Lesson6/map_func.py
@@ -1,13 +1,4 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 diction = {}
-something = list(map(lambda x: "even" if x % 2 == 0 else "odd", numbers))
-print(something)
-
-
-def some_activity(x, y):
-    for i in range(len(numbers)):
-        diction.update({x[i]: y[i]})
-
-
-some_activity(numbers, something)
+list(map(lambda x, y: diction.update({x: y}), numbers, list(map(lambda x: "even" if x % 2 == 0 else "odd", numbers))))
 print(diction)
